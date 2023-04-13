00d5d532f25dc91d90a1768ec23fff13560a47f9
Akash Kumar Sahu <130211152+akku2005@users.noreply.github.com>
2023-04-13 18:38:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const p_btns = document.querySelector('.p-btns');
+const p_btn = document.querySelectorAll('.p-btn');
+const p_img_elem = document.querySelectorAll('.img-overlay');
+
+p_btns.addEventListener('click',(e)=>{
+    const p_btn_clicked = e.target;
+    
+    p_btn.forEach((curElem)=>curElem.classList.remove("p-btn-active"));
+    p_btn_clicked.classList.add("p-btn-active");
+
+    const btn_num = p_btn_clicked.dataset.btnNum ;
+    // console.log(btn_num);
+
+
+    const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+
+    p_img_elem.forEach((curElem)=>curElem.classList.add('p-image-not-active'));
+
+    img_active.forEach((curElem) => curElem.classList.remove('p-image-not-active'));
+
+
+});
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView:3,
+    spaceBetween:30,
+    autoplay:{
+      delay:2500,
+      disableOnInteraction:false,
+    },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+      },
+    });
